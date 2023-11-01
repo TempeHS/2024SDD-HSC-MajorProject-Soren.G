5b841d1f33e3f113b897d4f053596e3707c3620b
SorenEdu <soren.gautam@education.nsw.gov.au>
2023-11-02 09:00:32 +1100

    Created the server
    Copied mr jones instructions

---
+++ myPWA/.database/myQuery.sql
@@ -0,0 +1,34 @@
+ /*CREATE TABLE sddstudents (nesaID INTEGER NOT NULL PRIMARY KEY, name TEXT NOT NULL, age INTEGER TEXT NOT NULL); */
+
+
+INSERT INTO sddstudents (nesaID, name, age) VALUES (22324, "David Smith", "17")
+
+INSERT INTO sddstudents (nesaID, name, age) VALUES (212325, "Smith Smith", "17")
+
+INSERT INTO sddstudents (nesaID, name, age) VALUES (22326, "Jake William", "17")
+
+INSERT INTO sddstudents (nesaID, name, age) VALUES (22327, "Steven Williams", "17")
+
+INSERT INTO sddstudents (nesaID, name, age) VALUES (22328, "Jacob rot", "17")
+
+INSERT INTO sddstudents (nesaID, name, age) VALUES (22329 "Zion james", "16")
+
+INSERT INTO sddstudents (nesaID, name, age) VALUES (22330 "Mohammed Zicks", "16")
+
+INSERT INTO sddstudents (nesaID, name, age) VALUES (22331 "Zack Salameh", "16")
+
+INSERT INTO sddstudents (nesaID, name, age) VALUES (22332 "Erling Haaland", "16")
+
+INSERT INTO sddstudents (nesaID, name, age) VALUES (22333 "Rasmus Hojlund", "16")
+
+INSERT INTO sddstudents (nesaID, name, age) VALUES (22334 "Bruno Guimeraes", "16")
+
+SELECT * FROM sddstudents;
+
+
+
+
+
+
+
+
